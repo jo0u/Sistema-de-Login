fb8e9f0d1e7a64ea4994e47ae94b91207c5b4250
Jou Alexandre <69989169+jo0u@users.noreply.github.com>
2021-10-21 12:57:25 -0300

    Update header.php

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="_imagens/favicon.ico" type="image/x-icon">
-    <title>Menu | SCF</title>
+    <title>Menu | Sistema de login</title>
     <link rel="stylesheet" href="_css/menu.css">
     <!-- Boxicons CSS -->
     <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
